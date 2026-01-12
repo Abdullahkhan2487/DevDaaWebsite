8e663f4c56829d7ae558227fd235a16e80e2f385
Abdullah Khan <i210835@nu.edu.pk>
2026-01-12 14:42:40 +0500

    memory cleaning

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -1,12 +1,7 @@
 "use client";
 
 import Tag from "@/components/Tag";
-import {
-    useMotionValue,
-    useMotionValueEvent,
-    useScroll,
-    useTransform,
-} from "framer-motion";
+import { useScroll, useTransform } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -26,21 +21,25 @@ export default function Introduction() {
     const wordIndex = useTransform(scrollYProgress, [0, 1], [0, words.length]);
 
     useEffect(() => {
-        wordIndex.on("change", (latest) => {
-            setCurrentWord(latest);
+        const unsubscribe = wordIndex.on("change", (latest) => {
+            setCurrentWord(Math.floor(latest));
         });
+
+        return () => {
+            unsubscribe();
+        };
     }, [wordIndex]);
 
     return (
-        <section className="py-28 lg:py-40  ">
+        <section className="py-28 lg:py-40">
             <div className="container">
                 <div className="sticky top-28 md:top-32">
                     <div className="flex justify-center">
                         <Tag>Introduction DevDaa</Tag>
                     </div>
-                    <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10 ">
+                    <div className="text-3xl md:text-5xl lg:text-6xl text-center font-medium mt-10">
                         <span>Your business deserves innovation&nbsp;</span>
-                        <span className="text-white/15 ">
+                        <span className="text-white/15">
                             {words.map((word, wordIndex) => (
                                 <span
                                     key={wordIndex}
